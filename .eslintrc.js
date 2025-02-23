d87349da782c34ea36dc35a7a5d26e372ba02e74
gagan-sudo <gaganbhangu34484@gmail.com>
2025-02-23 13:10:08 +0530

    verison 1 complete

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    settings: {
+      react: {
+        version: "detect" // Automatically detect the version of React
+      }
+    }
+  };
+  
---
+++ src/components/Contact.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import Button from '../components/Button'
+
+const ImageClipBox=({src,clipClass})=>(
+  <div className={`${clipClass}`}>
+            <img src={src}  />
+          </div>
+)
+
+const Contact = () => {
+  return (
+    <div id='contact' className='my-20 max-h-[40rem] w-screen px-10 '>
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
+          <ImageClipBox clipClass={"contact-clip-path-1"} src={"img/contact-1.webp"} />
+          <ImageClipBox clipClass={"contact-clip-path-2 lg:translate-y-40 translate-y-60"} src={"img/contact-2.webp"} />
+        </div>
+        <div className="absolute left-20 -top-40 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80 ">
+        <ImageClipBox clipClass={"swordman.webp md:scale-125"} src={"img/swordman.webp"} />
+       
+        </div>
+
+       <div className="flex flex-col items-center text-center ">
+         <p className="font-general text-[10px] uppercase">
+        Join Zentry
+        </p>
+
+        <p className=" spacial-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]  ">
+           Let&apos;s b<b>u</b>ild the <br/> new era <br/> of  gaming t<b>o</b>gether     
+        </p>
+        <Button title={"contact us"} containerClass={"mt-10 cursor-pointer"} /> 
+       </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Contact
